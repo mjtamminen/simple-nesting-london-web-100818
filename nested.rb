470333d6ac3bb0b2e32a9e01981e6ddb4c339ba0
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-10-02 06:16:50 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,7 +1,7 @@
 
 def hopper
-	programmer_hash = 
- 		{
+  programmer_hash = 
+    {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -15,9 +15,10 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+     
+     programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join
+     
 end
 
 def adding_matz
@@ -79,7 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  unshift.programmer_hash
     
 end
 
